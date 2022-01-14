e1b48d3374ff1555121bbfcf06e7b6168ec9c56f
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-01-14 13:10:05 -0600

    commit tree 8a0b6da22df9f79132df3d1afd6fec517d378085

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function findMatching(array, name){
+    const result = array.filter(elem => elem.toLowerCase() === name.toLowerCase());
+    //console.log(result);
+    
+    return result;
+}
+
+
+function fuzzyMatch(array, partName){
+    const result = array.filter(elem => elem.substring(0,2) === partName);
+    return result;
+}
+
+
+
+function matchName(array, string){
+    const result = array.filter(elem => elem.name === string);
+    console.log(result);
+    return result;
+}
